dce838e5519b3f2e58051dfdae3df504df413298
Matheus Nogueira <matheusnogueirad8@gmail.com>
2025-04-21 21:39:33 -0300

    Data Query V17

--- SQL/File--DQL--(Data_Query_L.)/DQL.sql
+++ SQL/File--DQL--(Data_Query_L.)/DQL.sql
@@ -1,9 +1,6 @@
-
 -- DQL (Data Query Language)
 -- Consultando os Dados - Como recuperar os dados inseridos
 
-SELECT * FROM MC_CLIENTE
--- ===============================================================================
 -- Exericio 01 
 -- -------------------------------------------------------------------------------
 /* Crie uma consulta SQL por meio do comando SELECT que exiba informações das categorias de produto e
@@ -14,92 +11,73 @@ Classifique a consulta por ordem de nome de categoria e nome de produto, ambos d
 */
 
 SELECT
-    CD_CATEGORIA,
-    NM_CATEGORIA,
-    CD_PRODUTO,
-    DS_PRODUTO,
-    VL_UNITARIO,
-    TP_EMBALAGEM,
-    VL_PERC_LUCRO
+    c.CD_CATEGORIA AS CODIGO_CATEGORIA,
+    c.DS_CATEGORIA AS NOME_CATEGORIA,
+    p.CD_PRODUTO AS CODIGO_PRODUTO,
+    p.DS_PRODUTO AS DESCRICAO_PRODUTO,
+    p.VL_UNITARIO AS VALOR_UNITARIO,
+    p.TP_EMBALAGEM AS TIPO_EMBALAGEM,
+    p.VL_PERC_LUCRO AS PERCENTUAL_LUCRO
 FROM 
-    MC_CATEGORIA c
+    MC_CATEGORIA_PROD c
 LEFT JOIN 
-    MC_PRODUTO p ON c.CD_CATEGORIA = p.CD_PRODUTO
-ORDER BY NM_CATEGORIA ASC, 
-    NM_PRODUTO ASC;
+    MC_PRODUTO p ON c.CD_CATEGORIA = p.CD_CATEGORIA
+ORDER BY 
+    c.DS_CATEGORIA ASC, 
+    p.DS_PRODUTO ASC;
 
--- ===============================================================================
+
 -- Exericio 02
 -- -------------------------------------------------------------------------------
 /* Crie uma instrução SQL que exiba os dados dos clientes pessoas físicas. Exiba as seguintes informações:
 código e nome do cliente, e-mail, telefone, login, data de nascimento no formato dia-mes-ano (com 4
 digitos), dia da semana da data de nascimento, anos de vida, sexo biológico e CPF.
 */
+
 SELECT 
-    c.NR_CLIENTE,
-    c.NM_CLIENTE,
-    c.DS_EMAIL,
-    c.NR_TELEFONE,
-    c.NM_LOGIN,
-    TO_CHAR(f.DT_NASCIMENTO, 'DD-MM-YYYY') AS DATA_NASCIMENTO_FORMATADA,
+    c.NR_CLIENTE AS CODIGO_CLIENTE,
+    c.NM_CLIENTE AS NOME_CLIENTE,
+    c.DS_EMAIL AS EMAIL,
+    c.NR_TELEFONE AS TELEFONE,
+    c.NM_LOGIN AS LOGIN,
+    TO_CHAR(f.DT_NASCIMENTO, 'DD-MM-YYYY') AS DATA_NASCIMENTO,
     TO_CHAR(f.DT_NASCIMENTO, 'DAY') AS DIA_SEMANA,
-    TRUNC(MONTHS_BETWEEN(SYSDATE, f.DT_NASCIMENTO) / 12) AS IDADE,
-    f.FL_SEXO_BIOLOGICO,
-    f.NR_CPF
+    TRUNC(MONTHS_BETWEEN(SYSDATE, f.DT_NASCIMENTO)/12) AS IDADE,
+    f.FL_SEXO_BIOLOGICO AS SEXO_BIOLOGICO,
+    f.NR_CPF AS CPF
 FROM 
     MC_CLIENTE c
-RIGHT JOIN 
-    MC_CLI_FISICA f ON f.NR_CLIENTE = c.NR_CLIENTE;
+JOIN 
+    MC_CLI_FISICA f ON c.NR_CLIENTE = f.NR_CLIENTE
+ORDER BY
+    c.NM_CLIENTE;
 
-
--- ===============================================================================
 -- Exericio 03
 -- -------------------------------------------------------------------------------
 /* Exiba as seguintes informações da tabela de visualização dos vídeos dos produtos: código do produto, nome
 do produto, data e hora de visualização do produto. Exiba essas informações classificadas pela data e hora
 mais recente. 
 */
---Meu codigo
-SELECT 
-    p.CD_PRODUTO,
-    p.NM_PRODUTO,
-    v.DT_VISUALIZACAO,
-    v.NR_HORA_VISUALIZACAO
-FROM MC_PRODUTO p
-RIGHT JOIN MC_SGV_VISUALIZACAO_VIDEO v ON v.NR_CLIENTE = p.NR_CLIENTE
-Order by DT_VISUALIZACAO, NR_HORA_VISUALIZACAO;
 
-
--- Chat gpt
 SELECT 
-    p.CD_PRODUTO,
-    p.DS_PRODUTO AS NM_PRODUTO,
-    v.DT_VISUALIZACAO,
-    v.NR_HORA_VISUALIZACAO
+    v.CD_PRODUTO AS CODIGO_PRODUTO,
+    p.DS_PRODUTO AS NOME_PRODUTO,
+    v.DT_VISUALIZACAO AS DATA_VISUALIZACAO,
+    TO_CHAR(
+        TO_DATE(
+            LPAD(v.NR_HORA_VISUALIZACAO, 2, '0') || ':' || 
+            LPAD(v.NR_MINUTO_VIDEO, 2, '0') || ':' || 
+            LPAD(v.NR_SEGUNDO_VIDEO, 2, '0'), 
+            'HH24:MI:SS'
+        ), 
+        'HH24:MI:SS'
+    ) AS HORA_VISUALIZACAO
 FROM 
     MC_SGV_VISUALIZACAO_VIDEO v
 JOIN 
-    MC_SGV_PRODUTO_VIDEO p ON v.CD_PRODUTO = p.CD_PRODUTO 
-    AND v.NR_SEQUENCIA = p.NR_SEQUENCIA
-JOIN MC_PRODUTO p ON p.CD_PRODUTO = p.CD_PRODUTO
+    MC_PRODUTO p ON v.CD_PRODUTO = p.CD_PRODUTO
 ORDER BY 
-    v.DT_VISUALIZACAO DESC, 
-    v.NR_HORA_VISUALIZACAO DESC;
-
-
-SELECT 
-    vv.CD_PRODUTO,
-    p.DS_PRODUTO AS NOME_PRODUTO,
-    vv.DT_VISUALIZACAO,
-    TO_CHAR(TO_DATE(vv.NR_HORA_VISUALIZACAO || ':' || vv.NR_MINUTO_VIDEO || ':' || vv.NR_SEGUNDO_VIDEO, 'HH24:MI:SS'), 'HH24:MI:SS') AS HORA_VISUALIZACAO
-FROM 
-    MC_SGV_VISUALIZACAO_VIDEO vv
-JOIN 
-    MC_PRODUTO p ON vv.CD_PRODUTO = p.CD_PRODUTO
-ORDER BY 
-    vv.DT_VISUALIZACAO DESC,
-    vv.NR_HORA_VISUALIZACAO DESC,
-    vv.NR_MINUTO_VIDEO DESC,
-    vv.NR_SEGUNDO_VIDEO DESC;
-
-
+    v.DT_VISUALIZACAO DESC,
+    v.NR_HORA_VISUALIZACAO DESC,
+    v.NR_MINUTO_VIDEO DESC,
+    v.NR_SEGUNDO_VIDEO DESC;
